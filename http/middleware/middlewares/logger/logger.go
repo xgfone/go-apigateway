731ce6759b393f3f78b3ecd68e46e90a2385674a
xgfone <xgfone@126.com>
2026-06-24 11:22:12 +0800

    refactor the http logger middleware

--- http/middleware/middlewares/logger/logger.go
+++ http/middleware/middlewares/logger/logger.go
@@ -16,42 +16,131 @@
 package logger
 
 import (
+	"context"
 	"log/slog"
 	"sync"
 	"time"
 
 	"github.com/xgfone/go-apigateway/http/core"
-	"github.com/xgfone/go-apigateway/http/middleware"
 	"github.com/xgfone/go-apigateway/http/statuscode"
 )
 
-var (
-	// Collect is used to collect the extra key-value attributes if set.
+// Config configures a request logger middleware handler.
+type Config struct {
+	// Enabled reports whether the request should be logged.
 	//
-	// Default: nil
-	Collect func(c *core.Context, append func(...slog.Attr))
+	// If nil, all requests are logged when the configured slog level is enabled.
+	Enabled func(*core.Context) bool
 
-	// Enabled is used to decide whether to log the request if set.
+	// GetRequestBody returns the request body written to the "reqbody"
+	// log attribute.
 	//
-	// Default: nil
-	Enabled func(c *core.Context) bool
-)
+	// If nil, the request body is omitted.
+	GetRequestBody func(*core.Context) any
 
-// Logger returns a logger middleware to log the http request.
-func Logger() middleware.Middleware { return logger }
+	// GetResponseBody returns the response body written to the "resbody",
+	//
+	// If nil, the response body is omitted.
+	GetResponseBody func(*core.Context) any
 
-var logger = middleware.New("logger", nil, func(next core.Handler) core.Handler {
-	return func(c *core.Context) { logreq(c, next) }
-})
+	// PostHandler is called after PreHandle is called and before the logger
+	// middleware handler ends.
+	//
+	// If nil, do nothing.
+	PostHandle func(*core.Context)
 
-func logreq(c *core.Context, next core.Handler) {
-	if (Enabled != nil && !Enabled(c)) || !slog.Default().Enabled(c.Context, slog.LevelInfo) {
-		next(c)
+	// PreHandler is called immediately before the next handler.
+	//
+	// If nil, do nothing.
+	PreHandle func(*core.Context)
+
+	// LogExtra appends the extra log attributes.
+	//
+	// If nil, do nothing.
+	LogExtra func(c *core.Context, append func(...slog.Attr))
+}
+
+// Logger return a new Logger.
+func (c Config) Logger() *Logger {
+	logger := &Logger{
+		enabled:         c.Enabled,
+		getRequestBody:  c.GetRequestBody,
+		getResponseBody: c.GetResponseBody,
+		postHandle:      c.PostHandle,
+		preHandle:       c.PreHandle,
+		logExtra:        c.LogExtra,
+		level:           slog.LevelInfo,
+	}
+	return logger
+}
+
+// NewDefaultConfig returns a new default Config.
+//
+//	Enabled: only ignore the root path "/".
+func NewDefaultConfig() Config {
+	return Config{
+		Enabled: enabled,
+	}
+}
+
+func enabled(c *core.Context) bool {
+	return c.ClientRequest.URL.Path != "/"
+}
+
+// Logger is a logger middleware handler.
+type Logger struct {
+	enabled         func(*core.Context) bool
+	getRequestBody  func(*core.Context) any
+	getResponseBody func(*core.Context) any
+	postHandle      func(*core.Context)
+	preHandle       func(*core.Context)
+	logExtra        func(*core.Context, func(...slog.Attr))
+
+	level slog.Level
+	next  core.Handler
+}
+
+// Name implements the interface middleware.Middleware#Name.
+func (l *Logger) Name() string {
+	return "logger"
+}
+
+// Handler implements the interface middleware.Middleware#Handler.
+func (l *Logger) Handler(next core.Handler) core.Handler {
+	if next == nil {
+		panic("Logger.Handler: next core.Handler is nil")
+	}
+
+	_l := *l
+	_l.next = next
+	return _l.Serve
+}
+
+func enableLevel(ctx context.Context, level slog.Level) bool {
+	return slog.Default().Enabled(ctx, level)
+}
+
+// Serve implements core.Handler.
+func (l *Logger) Serve(c *core.Context) {
+	if l.next == nil {
+		c.Abort(statuscode.ErrInternalServerError.WithMessage("Logger: NO NEXT HANDLER"))
 		return
 	}
 
+	if !(enableLevel(c.Context, l.level) && (l.enabled == nil || l.enabled(c))) {
+		l.next(c)
+		return
+	}
+
+	if l.preHandle != nil {
+		l.preHandle(c)
+	}
+	if l.postHandle != nil {
+		defer l.postHandle(c)
+	}
+
 	start := time.Now()
-	next(c)
+	l.next(c)
 	cost := time.Since(start)
 
 	req := c.ClientRequest
@@ -62,6 +151,7 @@ func logreq(c *core.Context, next core.Handler) {
 		slog.String("method", req.Method),
 		slog.String("host", req.Host),
 		slog.String("path", req.URL.Path),
+		slog.String("query", req.URL.RawQuery),
 	)
 
 	if c.RouteId != "" {
@@ -74,13 +164,22 @@ func logreq(c *core.Context, next core.Handler) {
 		}
 	}
 
+	if l.logExtra != nil {
+		l.logExtra(c, logattrs.Append)
+	}
+
 	logattrs.Append(
-		slog.Int("code", c.ClientResponse.StatusCode()),
 		slog.String("cost", cost.String()),
+		slog.Int("code", c.ClientResponse.StatusCode()),
+		slog.Any("reqheader", c.ClientRequest.Header),
+		slog.Any("resheader", c.ClientResponse.Header()),
 	)
 
-	if Collect != nil {
-		Collect(c, logattrs.Append)
+	if l.getRequestBody != nil {
+		logattrs.Append(slog.Any("reqbody", l.getRequestBody(c)))
+	}
+	if l.getResponseBody != nil {
+		logattrs.Append(slog.Any("resbody", l.getResponseBody(c)))
 	}
 
 	switch e := c.Error.(type) {
@@ -93,7 +192,7 @@ func logreq(c *core.Context, next core.Handler) {
 		logattrs.Append(slog.Any("err", c.Error))
 	}
 
-	slog.LogAttrs(c.Context, slog.LevelInfo, "log http request", logattrs.Attrs...)
+	slog.LogAttrs(c.Context, l.level, "log http request", logattrs.Attrs...)
 	putattrs(logattrs)
 }
 
--- http/middleware/middlewares/logger/logger_test.go
+++ http/middleware/middlewares/logger/logger_test.go
@@ -15,41 +15,65 @@
 package logger
 
 import (
+	"bytes"
 	"context"
-	"errors"
+	"encoding/json"
 	"log/slog"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/xgfone/go-apigateway/http/core"
-	"github.com/xgfone/go-apigateway/http/statuscode"
 	"github.com/xgfone/go-loadbalancer/endpoint"
 )
 
 func TestLogger(t *testing.T) {
-	Collect = func(c *core.Context, f func(...slog.Attr)) {
-		f(slog.String("test", "test"))
-	}
+	origLogger := slog.Default()
+	defer func() { slog.SetDefault(origLogger) }()
 
-	Enabled = func(c *core.Context) bool {
-		return c.ClientRequest.URL.Path != "/"
-	}
+	buf := bytes.NewBuffer(nil)
+	slog.SetDefault(slog.New(slog.NewJSONHandler(buf, nil)))
 
-	handler := Logger().Handler(func(c *core.Context) {})
+	config := NewDefaultConfig()
+	config.LogExtra = func(_ *core.Context, append func(...slog.Attr)) {
+		append(slog.String("test", "true"))
+	}
+	handler := config.Logger().Handler(func(c *core.Context) {})
 
 	rec := httptest.NewRecorder()
 	c := core.AcquireContext(context.Background())
+	c.ClientRequest = httptest.NewRequest("GET", "/path", nil)
 	c.ClientResponse = core.AcquireResponseWriter(rec)
-	c.ClientRequest = httptest.NewRequest("GET", "/", nil)
-	handler(c)
+	defer core.ReleaseResponseWriter(c.ClientResponse)
 
 	c.RouteId = "routeid"
 	c.UpstreamId = "upid"
 	c.Endpoint = endpoint.New("epid", nil)
-	c.ClientRequest = httptest.NewRequest("GET", "/path", nil)
 	handler(c)
 
-	Logger().Handler(func(c *core.Context) { c.Error = errors.New("test") })(c)
-	Logger().Handler(func(c *core.Context) { c.Error = statuscode.ErrBadGateway })(c)
-	Logger().Handler(func(c *core.Context) { c.Error = statuscode.ErrBadGateway.WithMessage("test") })(c)
+	type Log struct {
+		Level string `json:"level"`
+		Msg   string `json:"msg"`
+		Path  string `json:"path"`
+		Test  string `json:"test"`
+
+		RouteId    string `json:"route"`
+		UpstreamId string `json:"upstream"`
+	}
+
+	expect := Log{
+		Level: "INFO",
+		Msg:   "log http request",
+		Path:  "/path",
+		Test:  "true",
+
+		RouteId:    "routeid",
+		UpstreamId: "upid",
+	}
+
+	var actual Log
+	if err := json.Unmarshal(buf.Bytes(), &actual); err != nil {
+		t.Fatal(err)
+	} else if actual != expect {
+		t.Errorf("expect %+v, but got %+v", expect, actual)
+	}
 }
